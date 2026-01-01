ee22af8edcb5577b3f525b80e93b3eec7f5f3767
mulukenkassaw2025 <mulukenkassaw2025@gmail.com>
2026-01-01 09:29:22 +0300

    Initialze gradle

---
+++ src/main/java/com/example/demo/GradleTest1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTest1Application.class, args);
+		
+		System.out.println(" hello world");
+	}
+
+}
